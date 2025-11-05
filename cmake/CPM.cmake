5778ac2d14dab97c6b1c6d21a05de43260759a37
Craig Hutchinson <54269136+CraigHutchinson@users.noreply.github.com>
2025-11-05 10:24:26 +0000

    CPM user-profile caching by default

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -5,21 +5,15 @@
 set(CPM_DOWNLOAD_VERSION 0.42.0)
 set(CPM_HASH_SUM "2020b4fc42dba44817983e06342e682ecfc3d2f484a581f11cc5731fbe4dce8a")
 
-# Implicit CPM configure to use a user-pfoile CPM cache
+# CPM configure to use a user-profile caching by default
 # @note This can be overriden by defining CPM_SOURCE_CACHE or the CPM_SOURCE_CACHE environment variable
 if(NOT DEFINED CPM_SOURCE_CACHE AND NOT DEFINED ENV{CPM_SOURCE_CACHE})
-
-  if(WIN32)
-    set(_cpm_home "$ENV{USERPROFILE}/.cpm")
-  else()
-    set(_cpm_home "$ENV{HOME}/.cpm")
-  endif()
-  file(TO_CMAKE_PATH "${_cpm_home}" CPM_SOURCE_CACHE)
+  get_filename_component(CPM_SOURCE_CACHE "~/.cpm" ABSOLUTE)
   set(CPM_SOURCE_CACHE "${CPM_SOURCE_CACHE}" CACHE PATH "Download cache used by CPM")
-  message(WARNING "CPM_SOURCE_CACHE not defined; defaulting to ${CPM_SOURCE_CACHE}.
-   Set CPM_SOURCE_CACHE or the CPM_SOURCE_CACHE environment variable to override this location.")
+  message(STATUS  
+  "CPM_SOURCE_CACHE not defined; defaulting to: '${CPM_SOURCE_CACHE}'
+  Hint: Set CPM_SOURCE_CACHE or Env{CPM_SOURCE_CACHE} to override this location.")
 endif()
-
 
 if(CPM_PATH) # CPM_PATH may be provided on command-line
   set(CPM_DOWNLOAD_LOCATION "${CPM_PATH}/CPM.cmake")
